66ea67da34b100adfa243f9282be5f35eefd082c
kellerflint <kflint0068@gmail.com>
2019-03-10 18:26:23 -0700

    rebuilt login page and header nav styles; other small changes

---
+++ public_html/user/leaders.php
@@ -0,0 +1,16 @@
+<?php require_once '../../private/initialize.php'; ?>
+<?php //require_role(STU); ?>
+<?php 
+$page_title = 'Leaderboard';
+$page_style = 'leaderboard'; 
+?>
+
+<?php include_once SHARED_PATH . '/default_header.php'; ?>
+
+<div class="content">
+
+    <h2>Leaderboard</h2>
+
+</div>
+
+<?php include_once SHARED_PATH . '/default_footer.php'; ?>
