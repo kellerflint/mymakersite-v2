de9b56917da56e929a545b1bdb66512a5cb5464e
kellerflint <kflint0068@gmail.com>
2019-03-12 09:10:20 -0700

    added edit account

--- private/query_functions.php
+++ private/query_functions.php
@@ -16,6 +16,22 @@ function find_user_by_username($username)
     return mysqli_fetch_assoc($user_set);
 }
 
+function find_user_by_id($id)
+{
+    global $db;
+    $query = "SELECT * FROM User WHERE user_id = ?";
+    
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+    $user_set = $stmt->get_result();
+
+    $stmt->close();
+    
+    return mysqli_fetch_assoc($user_set);
+}
+
 function add_new_user($new_user) 
 {
     global $db;
@@ -35,7 +51,7 @@ function add_new_user($new_user)
         $new_user['user_last'], 
         $new_user['user_hashed_password'], 
         $new_user['user_email']);
-    $stmt->execute();
+    $result = $stmt->execute();
 
     if ($result) {
         return true;
@@ -44,38 +60,97 @@ function add_new_user($new_user)
     }
 }
 
-function validate_new_user($new_user) {
+function edit_user($user, $id) {
+global $db;
+
+    $errors = validate_edit_user($user);
+
+    if(!empty($errors)) {
+        return $errors;
+    }
+
+    $query = "UPDATE User SET user_name = ?, user_first = ?, user_last = ?,"; 
+    $query .= "user_email = ? WHERE user_id = ?";
+
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ssssi", 
+        $user['user_name'],
+        $user['user_first'],
+        $user['user_last'],
+        $user['user_email'],
+        $id);
+    $result = $stmt->execute();
+
+    if ($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validate_new_user($user) {
     $errors = [];
     
-    if (is_empty($new_user['user_first']))
+    if (is_empty($user['user_first']))
         $errors[] = "First name cannot be blank.";
-    if (is_empty($new_user['user_last']))
+    if (is_empty($user['user_last']))
         $errors[] = "Last name cannot be blank.";
-    if (is_empty($new_user['user_name']))
+    if (is_empty($user['user_name']))
         $errors[] = "Username cannot be blank.";
-    if (is_empty($new_user['user_email']))
+    if (is_empty($user['user_email']))
         $errors[] = "Email cannot be blank.";
-    if (is_empty($new_user['user_password']))
+    if (is_empty($user['user_password']))
         $errors[] = "Password cannot be blank.";
 
-    if (has_length_greater_than($new_user['user_first'], MAX_LENGTH))
+    if (has_length_greater_than($user['user_first'], MAX_LENGTH))
         $errors[] = "First name must be less than 256 characters.";
-    if (has_length_greater_than($new_user['user_last'], MAX_LENGTH))
+    if (has_length_greater_than($user['user_last'], MAX_LENGTH))
         $errors[] = "Last name must be less than 256 characters.";
-    if (has_length_greater_than($new_user['user_name'], MAX_LENGTH))
+    if (has_length_greater_than($user['user_name'], MAX_LENGTH))
         $errors[] = "Username must be less than 256 characters.";
-    if (has_length_greater_than($new_user['user_email'], MAX_LENGTH))
+    if (has_length_greater_than($user['user_email'], MAX_LENGTH))
         $errors[] = "Email must be less than 256 characters.";
-    if (has_length_greater_than($new_user['user_password'], MAX_LENGTH))
+    if (has_length_greater_than($user['user_password'], MAX_LENGTH))
         $errors[] = "Password must be less than 256 characters.";
 
-    if (has_length_less_than($new_user['user_password'], 6))
+    if (has_length_less_than($user['user_password'], 6))
         $errors[] = "Password must be at least 6 characters.";
 
-    if (!has_valid_email_format($new_user['user_email']))
+    if (!has_valid_email_format($user['user_email']))
         $errors[] = "Email must be valid.";
 
-    if (find_user_by_username($new_user['user_name']))
+    if (find_user_by_username($user['user_name']))
+        $errors[] = "The username you chose is already in use.";
+
+    return $errors;
+}
+
+function validate_edit_user($user) {
+    $errors = [];
+    
+    if (is_empty($user['user_first']))
+        $errors[] = "First name cannot be blank.";
+    if (is_empty($user['user_last']))
+        $errors[] = "Last name cannot be blank.";
+    if (is_empty($user['user_name']))
+        $errors[] = "Username cannot be blank.";
+    if (is_empty($user['user_email']))
+        $errors[] = "Email cannot be blank.";
+
+    if (has_length_greater_than($user['user_first'], MAX_LENGTH))
+        $errors[] = "First name must be less than 256 characters.";
+    if (has_length_greater_than($user['user_last'], MAX_LENGTH))
+        $errors[] = "Last name must be less than 256 characters.";
+    if (has_length_greater_than($user['user_name'], MAX_LENGTH))
+        $errors[] = "Username must be less than 256 characters.";
+    if (has_length_greater_than($user['user_email'], MAX_LENGTH))
+        $errors[] = "Email must be less than 256 characters.";
+
+    if (!has_valid_email_format($user['user_email']))
+        $errors[] = "Email must be valid.";
+
+    $found_user = find_user_by_username($user['user_name']);
+    if ($found_user && $found_user['user_name'] != $user['user_name'])
         $errors[] = "The username you chose is already in use.";
 
     return $errors;
--- public_html/account/account.php
+++ public_html/account/account.php
@@ -1,9 +1,56 @@
 <?php require_once '../../private/initialize.php' ?>
 <?php 
 $page_title = 'Account';
-$page_style = ''; 
+$page_style = 'account'; 
 ?>
 
 <?php include_once '../../private/shared/default_header.php'; ?>
+
+<?php 
+
+$user = find_user_by_id($_SESSION['user_id']);
+
+if(request_is_post()) {
+    
+    $user['user_name'] = hsc($_POST['username']);
+    $user['user_first'] = hsc($_POST['firstname']);
+    $user['user_last'] = hsc($_POST['lastname']);
+    $user['user_email'] = hsc($_POST['email']);
+    
+    $result = edit_user($user, $_SESSION['user_id']);
+
+    if ($result !== true) {
+        $errors = $result;
+    }
+} 
+?>
+
+<div class="content">
+
+    <h2>Edit Account</h2>
+
+    <?php echo display_errors($errors); ?>
+
+    <form action="account.php" method="POST">
+        <label for="firstname">First Name</label>
+        <br>
+        <input type="text" name="firstname" id="firstname" value="<?php echo $user['user_first']; ?>">
+        <br>
+        <label for="lastname">Last Name</label>
+        <br>
+        <input type="text" name="lastname" id="lastname" value="<?php echo $user['user_last']; ?>">
+        <br>
+        <label for="username">Username</label>
+        <br>
+        <input type="text" name="username" id="username" value="<?php echo $user['user_name']; ?>">
+        <br>
+        <label for="email">Email</label>
+        <br>
+        <input type="text" name="email" id="email" value="<?php echo $user['user_email']; ?>">
+        <br>
+        <button name="submit" id="submitBtn">Submit</button>
+    </form>
+
+</div>
 
 <?php include_once '../../private/shared/default_footer.php'; ?>
